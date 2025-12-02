d35c85c6ef8c2ac3f315b94d66008c3c1f4ee1fa
phatch9 <phat.chauu26@gmail.com>
2025-12-01 16:45:11 -0800

    check SavedPost - refactor

---
+++ browser/src/pages/Saved/SavedPost.tsx
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+import InfinitePosts from "../components/InfinitePosts";
+
+export default function SavedPosts() {
+    useEffect(() => {
+        document.title = "Reddot | saved";
+        return () => {
+            document.title = "Reddot";
+        };
+    }, []);
+
+    return (
+        <div className="flex flex-col items-center p-2 w-full">
+            <h2 className="text-3xl font-bold text-gray-800 mb-6 w-full max-w-2xl mx-auto">
+                Saved Posts
+            </h2>
+
+            <InfinitePosts
+                apiQueryKey="saved"
+                linkUrl="posts/saved"
+                forSaved={true}
+            />
+        </div>
+    );
+}
