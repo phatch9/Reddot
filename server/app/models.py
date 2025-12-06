f7a0fbd04447058fbd6290427f223b13f0d9829d
Phat <phat.chauu26@gmail.com>
2025-12-05 23:47:56 -0800

    server/app {models} - from User class updated attributes

--- server/app/models.py
+++ server/app/models.py
@@ -15,23 +15,23 @@ class UserRole(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False, primary_key=True)
     role_id = db.Column(db.Integer, db.ForeignKey("roles.id"), nullable=False, primary_key=True)
-    subthread_id = db.Column(db.Integer, db.ForeignKey("subthreads.id"))
+    subpost_id = db.Column(db.Integer, db.ForeignKey("subposts.id"))
     user = db.relationship("User", back_populates="user_role")
     role = db.relationship("Role", back_populates="user_role")
     created_at = db.Column(db.DateTime(timezone=True), nullable=False, default=db.func.now())
-    subthread = db.relationship("Subthread", back_populates="user_role")
+    subpost = db.relationship("SubPost", back_populates="user_role")
 
-    def __init__(self, user_id: int, subthread_id: int, role_id: int) -> None:
+    def __init__(self, user_id: int, subpost_id: int, role_id: int) -> None:
         self.user_id = user_id
-        self.subthread_id = subthread_id
+        self.subpost_id = subpost_id
         self.role_id = role_id
 
     @classmethod
-    def add_moderator(cls, user_id, subthread_id):
-        check_mod = UserRole.query.filter_by(user_id=user_id, subthread_id=subthread_id, role_id=1).first()
+    def add_moderator(cls, user_id, subpost_id):
+        check_mod = UserRole.query.filter_by(user_id=user_id, subpost_id=subpost_id, role_id=1).first()
         if check_mod:
-            return jsonify({"message": "Moderator already exists"}), 400
-        new_mod = UserRole(user_id=user_id, subthread_id=subthread_id, role_id=1)
+            return jsonify({"message": "Mod already exists"}), 400
+        new_mod = UserRole(user_id=user_id, subpost_id=subpost_id, role_id=1)
         db.session.add(new_mod)
         db.session.commit()
 
@@ -40,5 +40,5 @@ class UserRole(db.Model):
             "id": self.id,
             "user_id": self.user_id,
             "role_id": self.role_id,
-            "subthread_id": self.subthread_id,
+            "subpost_id": self.subpost_id,
         }
